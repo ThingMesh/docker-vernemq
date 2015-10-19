59e3ce6b48506af130352dec1272f40429f13614
Lars Boegild Thomsen <lth@bright-things.com>
2015-10-19 04:08:54 +0200

    Hmmm

--- run.sh
+++ run.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 [ -f /etc/vernemq/vernemq.conf ] || cp /tmp/vernemq.conf.default /etc/vernemq/vernemq.conf
-[ -f /etc/vernemq/vmq.acl.default ] || cp /tmp/vmq.acl.default /etc/vernemq/vmq.acl
-[ -f /etc/vernemq/vmq.passwd.default ] || cp /tmp/vmq.passwd.default /etc/vernemq/vmq.passwd
+[ -f /etc/vernemq/vmq.acl ] || cp /tmp/vmq.acl.default /etc/vernemq/vmq.acl
+[ -f /etc/vernemq/vmq.passwd ] || cp /tmp/vmq.passwd.default /etc/vernemq/vmq.passwd
 
 vernemq console
+
